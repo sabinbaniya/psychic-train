6733a8445e15153da955b2cc409ed449886c0ce3
sabinbaniya <dummymail@tutanota.de>
2022-04-18 15:47:50 +0545

    authorization done

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,4 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt, { Secret } from "jsonwebtoken";
 
-export default function middleware(req: NextRequest) {}
+const middleware = async (req: NextRequest) => {
+  const token = req.cookies.access;
+
+  const url = req.nextUrl.clone();
+
+  if (!token) {
+    if (url.pathname === "/") {
+      return NextResponse.redirect(new URL("/login", url));
+    }
+    return NextResponse.next();
+  } else {
+    try {
+      const isValid = jwt.verify(token, process.env.JWT_SECRET as Secret);
+      if (url.pathname === "/login" || url.pathname === "/signup") {
+        if (isValid) {
+          return NextResponse.redirect(new URL("/", url));
+        }
+      }
+      return NextResponse.next();
+    } catch (error) {
+      if (url.pathname === "/") {
+        return NextResponse.redirect(new URL("/login", url));
+      }
+
+      return NextResponse.next();
+    }
+  }
+};
+
+export default middleware;
