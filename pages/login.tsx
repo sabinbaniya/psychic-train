5f9186812044691b9d68b6442c50e92745b89ae6
sabinbaniya <dummymail@tutanota.de>
2022-04-18 10:21:07 +0545

    login done

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,43 +5,27 @@ import { useForm } from "react-hook-form";
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 import axiosInstance from "../axios/axiosInstance";
 
-const signup = () => {
+const login = () => {
   const {
     register,
     handleSubmit,
     watch,
     reset,
-    setError,
     formState: { errors },
   } = useForm({
     defaultValues: {
       name: "",
       email: "",
       password: "",
-      cPassword: "",
     },
   });
 
   const router = useRouter();
 
-  const [showPassword, setShowPassword] = useState({ p: false, cp: false });
+  const [showPassword, setShowPassword] = useState(false);
 
   const submit = async () => {
-    const password = watch("password");
-    const cPassword = watch("cPassword");
-
-    const doesPasswordMatch = password === cPassword;
-
-    if (!doesPasswordMatch) {
-      setError("cPassword", {
-        type: "custom",
-        message: "Passwords don't match",
-      });
-      return;
-    }
-
     const data = {
-      name: watch("name"),
       email: watch("email"),
       password: watch("password"),
     };
@@ -50,11 +34,11 @@ const signup = () => {
 
     try {
       const res = await axiosInstance.post(
-        "/api/auth/signup",
+        "/api/auth/login",
         JSON.stringify(data)
       );
 
-      if (res.status === 201) {
+      if (res.status === 200) {
         return router.push("/");
       }
     } catch (error) {}
@@ -68,26 +52,11 @@ const signup = () => {
       <div className='min-h-screen grid place-items-center '>
         <div className='w-11/12 mx-auto'>
           <h1 className='font-bold text-2xl text-center'>
-            Signup | Create a new Account
+            Login | Login to your Account
           </h1>
           <form
             className='my-12 max-w-md mx-auto'
             onSubmit={handleSubmit(submit)}>
-            <div className='form_div '>
-              <label htmlFor='name' className='font-medium text-gray-900'>
-                Name
-              </label>
-              <input
-                type='text'
-                {...register("name", { required: "Please enter your name" })}
-                id='name'
-                autoComplete='off'
-                className='form_input'
-              />
-              {errors.name && (
-                <p className='form_error'>{errors.name.message}</p>
-              )}
-            </div>
             <div className='form_div'>
               <label htmlFor='email' className='font-medium text-gray-900'>
                 Email
@@ -116,7 +85,7 @@ const signup = () => {
               </label>
               <div className='w-full relative'>
                 <input
-                  type={`${showPassword.p ? "text" : "password"}`}
+                  type={`${showPassword ? "text" : "password"}`}
                   {...register("password", {
                     required: "Please enter your password",
                     minLength: {
@@ -132,19 +101,15 @@ const signup = () => {
                   autoComplete='off'
                   className='form_input'
                 />
-                {showPassword.p ? (
+                {showPassword ? (
                   <AiOutlineEyeInvisible
                     className='cursor-pointer absolute bottom-2 right-2'
-                    onClick={() =>
-                      setShowPassword({ ...showPassword, p: !showPassword.p })
-                    }
+                    onClick={() => setShowPassword((prev) => !prev)}
                   />
                 ) : (
                   <AiOutlineEye
                     className='cursor-pointer absolute bottom-2 right-2'
-                    onClick={() =>
-                      setShowPassword({ ...showPassword, p: !showPassword.p })
-                    }
+                    onClick={() => setShowPassword((prev) => !prev)}
                   />
                 )}
               </div>
@@ -153,47 +118,10 @@ const signup = () => {
                 <p className='form_error'>{errors.password.message}</p>
               )}
             </div>
-            <div className='form_div relative'>
-              <label
-                htmlFor=' my-4 space-y-1confirmPassword '
-                className='font-medium text-gray-900'>
-                Confirm Password
-              </label>
-              <div className='w-full relative'>
-                <input
-                  type={`${showPassword.cp ? "text" : "password"}`}
-                  {...register("cPassword", {
-                    required: "Please re-enter your password",
-                  })}
-                  id='confirmPassword'
-                  autoComplete='off'
-                  className='form_input'
-                />
-
-                {showPassword.cp ? (
-                  <AiOutlineEyeInvisible
-                    className='cursor-pointer absolute bottom-2 right-2'
-                    onClick={() =>
-                      setShowPassword({ ...showPassword, cp: !showPassword.cp })
-                    }
-                  />
-                ) : (
-                  <AiOutlineEye
-                    className='cursor-pointer absolute bottom-2 right-2'
-                    onClick={() =>
-                      setShowPassword({ ...showPassword, cp: !showPassword.cp })
-                    }
-                  />
-                )}
-              </div>
-              {errors.cPassword && (
-                <p className='form_error'>{errors.cPassword.message}</p>
-              )}
-            </div>
             <div className='grid place-items-center'>
               <input
                 type='submit'
-                value='Sign Up'
+                value='Log In'
                 className='my-4 px-4 py-2 rounded-md bg-gradient-to-bl from-blue-400 hover:from-blue-300  to-blue-600 hover:to-blue-500 text-white font-bold tracking-wider cursor-pointer'
               />
             </div>
@@ -204,4 +132,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default login;
