20ae2ab6fcd7dff9d20f75c2ef348a9dccdd026c
sabinbaniya <dummymail@tutanota.de>
2022-04-30 13:29:04 +0545

    get all friend feature added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,11 +4,22 @@ import {
   Dispatch,
   SetStateAction,
   SyntheticEvent,
+  useEffect,
+  useState,
 } from "react";
+import axiosInstance from "../../axios/axiosInstance";
 
 interface IUserInfo {
   name: string;
   status: string;
+}
+
+interface IFriendsList {
+  userId: string;
+  name: string;
+  email: string;
+  avatarUrl: string;
+  onlineStatus: boolean;
 }
 interface props {
   setSelectedUser: (arg: string) => void;
@@ -21,11 +32,43 @@ const Sidebar = ({ setSelectedUser, setUserInfo }: props) => {
     setUserInfo({ name: "Jhon Doe", status: "online" });
   };
 
+  const handleChange = (arg: string) => {
+    if (arg === "") {
+      return setFriendsList(mainFriendsList);
+    }
+
+    const list = friendsList?.filter((friend) => {
+      return friend.name.startsWith(arg);
+    });
+    setFriendsList(list);
+  };
+
+  const [friendsList, setFriendsList] = useState<IFriendsList[] | undefined>(
+    undefined
+  );
+
+  const [mainFriendsList, setMainFriendsList] = useState<
+    IFriendsList[] | undefined
+  >(undefined);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axiosInstance.get("/api/chat/getAllFriends");
+      setMainFriendsList(res.data);
+      setFriendsList(res.data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <div className='basis-1/3 max-w-xs bg-gray-100 h-[90vh] w-full overflow-y-auto'>
-      <form className='w-full h-12 grid place-items-center border-bottom-1'>
+      <form
+        className='w-full h-12 grid place-items-center border-bottom-1'
+        onSubmit={(e) => e.preventDefault()}>
         <input
           autoComplete='off'
+          onChange={(e) => handleChange(e.target.value)}
           type='text'
           name='query'
           id='user'
@@ -33,20 +76,26 @@ const Sidebar = ({ setSelectedUser, setUserInfo }: props) => {
           className='border-2 border-gray-400 outline-gray-500 rounded-full h-8 px-2 w-10/12 mx-auto'
         />
       </form>
-      <div
-        className='flex h-20 border-b-2 space-x-4 w-11/12 mx-auto space-y-1 items-center cursor-pointer'
-        onClick={() => handleClick("232134354325346a")}>
-        <div className=''>
-          <Image
-            src='/avatar.png'
-            height='40px'
-            width='40px'
-            className='rounded-full bg-green-100'></Image>
-        </div>
-        <div>
-          <p>Jhon Doe</p>
-          <p className='text-gray-500'>Hey! Welcome</p>
-        </div>
+      <div>
+        {friendsList &&
+          friendsList.map((friend) => (
+            <div
+              className='flex h-20 border-b-2 space-x-4 w-11/12 mx-auto space-y-1 items-center cursor-pointer'
+              onClick={() => handleClick("232134354325346a")}
+              key={friend.userId}>
+              <div className=''>
+                <Image
+                  src={friend.avatarUrl}
+                  height='40px'
+                  width='40px'
+                  className='rounded-full bg-green-100'></Image>
+              </div>
+              <div>
+                <p>{friend.name}</p>
+                <p className='text-gray-500'>{friend.onlineStatus}</p>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
